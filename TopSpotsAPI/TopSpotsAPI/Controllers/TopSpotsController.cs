a25467c86ad978de8289319c7bb701bdc981bb6a
blargo1 <bennie.largo@gmail.com>
2016-10-24 17:19:00 -0700

    First commit

---
+++ TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        // GET: api/TopSpots
+        public IEnumerable<TopSpot> Get()
+        {
+            // read file into a string and deserialize JSON to a type
+            var info = JsonConvert.DeserializeObject<IEnumerable<TopSpot>>(File.ReadAllText("C:/dev/TopSpotsAPI/topspots.json"));
+            return info;
+        }
+
+        // GET: api/TopSpots/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TopSpots
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TopSpots/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TopSpots/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
